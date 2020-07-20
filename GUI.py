1c6218c266fcbe3adafd1d9b7ad0d089adc6cc84
Nimnim12 <norbert.wojciechowski1995@gmail.com>
2020-07-20 20:23:23 +0200

    Added functions to show numbers on board

--- GUI.py
+++ GUI.py
@@ -1,4 +1,5 @@
 import pygame
+from sudoku_solver import example_board
 
 def create_window(width,height):
     # Create first window
@@ -6,7 +7,7 @@ def create_window(width,height):
     pygame.display.set_caption("Sudoku")
     return screen
 
-def draw_board(screen):
+def draw_lines(screen):
     width,height = pygame.display.get_surface().get_size()
     # draw vertical lines
     for i in range (1,9):
@@ -24,6 +25,18 @@ def draw_board(screen):
         if i%3 == 0:
             pygame.draw.line(screen, (0, 0, 0), (0, i*(height//9)), (width, i*(height//9)), 5)
 
+def draw_number(screen,row,col,value):
+    width, height = pygame.display.get_surface().get_size()
+    font = pygame.font.SysFont("ComicSans",150,True)
+    number = font.render(str(value),1,(0,0,0))
+    screen.blit(number,((col*(width//9))+(width//40),(row*(height//9))+(height//180)))
+
+def draw_board(screen,board):
+    for i in range(9):
+        for j in range(9):
+            if(board[i][j] != 0):
+                draw_number(screen,i,j,board[i][j])
+
 
 def main():
     screen = create_window(1200,900)
@@ -32,7 +45,8 @@ def main():
     while(running):
         # Background options
         screen.fill((250,250,250))
-        draw_board(screen)
+        draw_lines(screen)
+        draw_board(screen,example_board)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running=False
